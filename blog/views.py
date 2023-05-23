d4383259925ed31a0c38d4bca0966c0682e80612
Riad Shalahin Leon <105214634+riadleon@users.noreply.github.com>
2023-05-24 00:05:40 +0600

    learning django day 2 templating

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+    'author': 'Leon',
+    'title': 'Blog post 1',
+    'content': 'First Post Content',
+    'date_posted': 'June 27, 2023 '
+},
+{
+    'author': 'Corey',
+    'title': 'Blog post 2',
+    'content': 'second Post Content',
+    'date_posted': 'August 27, 2023 '
+},
+
+]
+
 def home(request):
-    return HttpResponse('<H1>Blog Home</H1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<H1>Blog About</H1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
