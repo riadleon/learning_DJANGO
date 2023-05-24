8fd804ddb32cd3f5b09258a855e4542f3621b209
Riad Shalahin Leon <105214634+riadleon@users.noreply.github.com>
2023-05-25 00:27:50 +0600

    learning django day 3 - Database and migrations

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-posts = [
-    {
-    'author': 'Leon',
-    'title': 'Blog post 1',
-    'content': 'First Post Content',
-    'date_posted': 'June 27, 2023 '
-},
-{
-    'author': 'Corey',
-    'title': 'Blog post 2',
-    'content': 'second Post Content',
-    'date_posted': 'August 27, 2023 '
-},
-
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
