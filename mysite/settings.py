c6b1425dc85f7f71574bd0ade30f9a782afca4f6
Riad Shalahin Leon <105214634+riadleon@users.noreply.github.com>
2023-05-26 22:52:11 +0600

    learning django day 5 - profile info and picture created

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -121,6 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 # Default primary key field type
--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from users import views as user_views
 
 urlpatterns = [
@@ -28,4 +30,11 @@ urlpatterns = [
     path('', include('blog.urls')),
 
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+
+    
 # for ppl trying to use bootstrap5, you need to add 'crispy_bootstrap5' to installed apps too, and also add CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5' in the main project's settings.py file
